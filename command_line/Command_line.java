eeb1bdf2a180dd7d6d268e412f477387d753b0bb
shane <hunghsuyong114shane@gmail.com>
2020-12-02 10:49:48 +0800

    for-each & command line argument

---
+++ command_line/Command_line.java
@@ -0,0 +1,7 @@
+public class Command_line{
+	public static void main(String[] args){
+		for(String command:args){
+			System.out.println(command);
+		}
+	}
+}
---
+++ for_each/For_try.java
@@ -0,0 +1,8 @@
+public class For_try{
+	public static void main(String[] args){
+		int[] scores={10,20,30,40};
+		for(int score:scores){
+			System.out.println(score);
+		}
+	}
+}
