25bb164a2e7cea48d35042d4567c323e32ca26be
shane <hunghsuyong114shane@gmail.com>
2020-12-03 12:52:28 +0800

    oop test

---
+++ OOP/Main.java
@@ -0,0 +1,7 @@
+class Main{
+	public static void main(String[] args){
+		MyClass test = new MyClass();
+		System.out.println("Sum of the attributions: " + test.add() );
+		System.out.println("The attribute x in MyClass is " + test.x);
+	}
+}
---
+++ OOP/MyClass.java
@@ -0,0 +1,7 @@
+class MyClass{
+	static int x=2;
+	static int y=4;
+	static int add(){
+		return x+y;
+	}
+}
