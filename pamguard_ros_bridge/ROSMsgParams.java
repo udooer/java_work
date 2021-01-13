8a89d916374b48a2883ac15fa7b4578990f49e44
shane <hunghsuyong114shane@gmail.com>
2021-01-13 18:13:10 +0800

    create the button to connect to server in daqpanel

---
+++ pamguard_ros_bridge/ROSMsgParams.java
@@ -0,0 +1,12 @@
+package pamguard_ros_bridge;
+/*
+ *  create a class to store the global varible 
+ *  which is user in this package.
+ */
+
+// import java_websocket lib
+import org.java_websocket.client.WebSocketClient;
+public class ROSMsgParams{
+ 	public WebSocketClient m_ws;
+ 	public boolean m_status = false;
+}
