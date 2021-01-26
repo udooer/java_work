dc82c6f3b48bf9386dbcbef73b29309acd82f401
shane <hunghsuyong114shane@gmail.com>
2021-01-26 17:13:29 +0800

    rearrange the filesystem of java_work

--- javatpoint/Thread/ThreadTest.java
+++ javatpoint/Thread/ThreadTest.java
@@ -1,16 +1,16 @@
 package Thread;
 
 public class ThreadTest implements Runnable{
-	public void run(){
-		System.out.println("thread is running");
-	}
-	public static void main(String[] args){
-		ThreadTest test = new ThreadTest();
-		Thread t = new Thread(test);
-		t.start();
-		try{Thread.sleep(1000);
-		}catch(Exception e){
-		}
-		System.out.println("out of thread run function");
-	}
+    public void run(){
+        System.out.println("thread is running");
+    }
+    public static void main(String[] args){
+        ThreadTest test = new ThreadTest();
+        Thread t = new Thread(test);
+        t.start();
+        try{Thread.sleep(1000);
+        }catch(Exception e){
+        }
+        System.out.println("out of thread run function");
+    }
 }
